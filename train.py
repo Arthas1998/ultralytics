34fdc01c52fb47804104a9a4d13464f7c2432875
wuweihang <wuweihang1998@foxmail.com>
2025-07-22 10:06:49 +0800

    first commit

---
+++ train.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+# @File:    train
+# @Time:    2025/7/22 10:03
+# @Author:  ArthasMenethil/wuweihang
+# @Contact: wuweihang1998@gmail.com
+# @Version: V 0.1
+def print_hello(name):
+    print(f'Hello,{name}')
+
+
+if __name__ == '__main__':
+    print_hello('World')
